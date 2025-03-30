d3bf41877c6d205ff3b87897ae3396ae1afaa070
awoiibob <awoiibobwilly@gmail.com>
2025-03-30 21:06:06 +0300

    PASSED test with other numbers

--- unittestenv/tddunittest.py
+++ unittestenv/tddunittest.py
@@ -9,6 +9,23 @@ class TestMultiplyAll(unittest.TestCase):
 
     def test_multiplyall1(self):
         self.assertEqual(multiplyall(1, 1), 1)
+        self.assertEqual(multiplyall(-1, 1), -1)
+        self.assertEqual(multiplyall(-1, -1), 1)
+
+    def test_multiplyall2(self):
+        self.assertEqual(multiplyall(2, 2), 4)
+        self.assertEqual(multiplyall(-2, 2), -4)
+        self.assertEqual(multiplyall(-2, -2), 4)
+
+    def test_multiplyall3(self):
+        self.assertEqual(multiplyall(3, 3), 9)
+        self.assertEqual(multiplyall(-3, 3), -9)
+        self.assertEqual(multiplyall(-3, -3), 9)
+
+    def test_multiplyall4(self):
+        self.assertEqual(multiplyall(41, 14), 41*14)
+        self.assertEqual(multiplyall(-41, 14), -41*14)
+        self.assertEqual(multiplyall(-41, -14), 41*14)
 
 
 if __name__ == '__main__':
