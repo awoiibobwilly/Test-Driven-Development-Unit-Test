8a7fdfda58a75588f117c8e75da1d2cfb653159b
awoiibob <awoiibobwilly@gmail.com>
2025-03-30 21:01:17 +0300

    This test has PASSED

--- unittestenv/tddunittest.py
+++ unittestenv/tddunittest.py
@@ -8,7 +8,7 @@ def multiplyall(a, b):
 class TestMultiplyAll(unittest.TestCase):
 
     def test_multiplyall1(self):
-        self.assertEqual(multiplyall(1, 1), 4)
+        self.assertEqual(multiplyall(1, 1), 1)
 
 
 if __name__ == '__main__':
