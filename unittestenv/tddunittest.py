b53236cd9f1c37ad654ca76399fa01c6dadf29b2
awoiibob <awoiibobwilly@gmail.com>
2025-03-30 20:42:15 +0300

    With ERROR

---
+++ unittestenv/tddunittest.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestMultiplyAll(unittest.TestCase):
+
+    def test_multiplyall1(self):
+        self.assertEqual((1, 1), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
