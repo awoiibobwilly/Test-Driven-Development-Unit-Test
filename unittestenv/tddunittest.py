67af5d3727c1678258381fbbe73532a28bb04e2e
awoiibob <awoiibobwilly@gmail.com>
2025-03-30 20:58:52 +0300

    This a FAILED TEST

--- unittestenv/tddunittest.py
+++ unittestenv/tddunittest.py
@@ -1,10 +1,14 @@
 import unittest
 
 
+def multiplyall(a, b):
+    return a*b
+
+
 class TestMultiplyAll(unittest.TestCase):
 
     def test_multiplyall1(self):
-        self.assertEqual((1, 1), 1)
+        self.assertEqual(multiplyall(1, 1), 4)
 
 
 if __name__ == '__main__':
